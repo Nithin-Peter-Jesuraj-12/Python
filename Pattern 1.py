5231f21120f162ba2744d59cf71e9f7586d60642
Nithin-Peter-Jesuraj-12 <53636848+Nithin-Peter-Jesuraj-12@users.noreply.github.com>
2021-06-13 12:12:45 +0530

    Update Pattern 1.py

--- Pattern 1.py
+++ Pattern 1.py
@@ -1,5 +1,7 @@
 a=int(input("Enter a number: "))
-for i in range(0,a):
-    for j in range(0,i+1):
-        print("* ",end="")
-    print("\r")
+#for i in range(0,a):
+    #for j in range(0,i+1):
+        #print("* ",end="")
+    #print("\r")
+for n in range(a+1):
+    print("*"*n)
